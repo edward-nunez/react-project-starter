faa229e22ee24209f423ca937a5906daf0fca762
dotRollen <edwardnnz@gmail.com>
2019-09-28 02:10:44 -0400

    [ ＼\ ٩( ᐛ )و /／ ] Initial express server files

---
+++ src/server/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(helmet());
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use('/', routes);
+
+module.exports = app;
